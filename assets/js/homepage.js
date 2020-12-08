5f511b588093dcec56b5b8cee189e3092b6142af
Matt Damron <mattdamron@msn.com>
2020-12-07 21:11:29 -0700

    add username form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,33 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    // check if there is input
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }
+    else {
+        alert('Please enter a GitHub username');
+    }
+};
+
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        console.log(response);
         response.json().then(function (data) {
             console.log(data);
         });
     });
-    // console.log('outside');
 };
 
-getUserRepos();
+userFormEl.addEventListener('submit', formSubmitHandler);
